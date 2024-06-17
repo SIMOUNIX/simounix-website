516931a3595bcfc3be5c535acdd97523b76d7298
Simon THUAUD <simounixx@gmail.com>
2024-06-17 08:33:15 +0200

    fix: responsive navbar and about content

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
+import { RouterLink, RouterView, useRouter } from 'vue-router';
+
+const toggleMenu = ref(false);
+
+const toggleMenuHandler = () => {
+  toggleMenu.value = !toggleMenu.value;
+};
+
+const closeMenu = () => {
+  toggleMenu.value = false;
+};
 </script>
 
 <template>
   <header>
     <div class="wrapper-nav">
       <nav class="main-nav">
-        <RouterLink to="/" class="nav-element home-route">simon thuaud</RouterLink>
-        <RouterLink to="/about" class="nav-element to-hide">à propos</RouterLink> 
-        <RouterLink to="/work" class="nav-element to-hide">travaux</RouterLink>
-        <RouterLink to="/recipes" class="nav-element to-hide">recettes</RouterLink>
+        <div class="logo">
+          <RouterLink to="/" class="nav-element home-route" @click="closeMenu">simon thuaud</RouterLink>
+        </div>
+        <div :class="['router', toggleMenu ? 'open-menu' : 'closed-menu']">
+          <RouterLink to="/about" class="nav-element" @click="closeMenu">à propos</RouterLink> 
+          <RouterLink to="/work" class="nav-element" @click="closeMenu">travaux</RouterLink>
+          <RouterLink to="/recipes" class="nav-element" @click="closeMenu">recettes</RouterLink>
+        </div>
+        <div class="burger" @click="toggleMenuHandler">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
       </nav>
     </div>
   </header>
--- website/src/views/RecipesView.vue
+++ website/src/views/RecipesView.vue
@@ -1,9 +1,7 @@
 <!-- change the website title to add - recipes -->
 
 <template>
-  <div class="recipes">
-    <h1>This is an about page</h1>
-  </div>
+  work in progress...
 </template>
 
 <style>
