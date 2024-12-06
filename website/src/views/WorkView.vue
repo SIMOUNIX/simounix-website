88caf569fa37459b257d900ff0db79eee0a8b561
THUAUD Simon <simounixx@gmail.com>
2024-12-06 11:17:24 +0100

    feat: styling the project card

--- website/src/views/WorkView.vue
+++ website/src/views/WorkView.vue
@@ -1,40 +1,53 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import SmallCard from '../components/ProjectSmallCard.vue';
 
+// const router = useRouter(); // not used yet
 const projects = ref([
-    {
-        id: 1,
-        image: new URL('../assets/images/epita.png', import.meta.url).href,
-        title: 'EPITA',
-        description: 'Actuellement étudiant en informatique à EPITA, je me spécialise en Intelligence Artificielle et Machine Learning.',
-        link: 'https://www.epita.fr',
-        linkText: 'EPITA',
-    },
-    {
-        id: 2,
-        image: new URL('../assets/images/ai.png', import.meta.url).href,
-        title: 'Intelligence Artificielle',
-        description: 'Passionné par l\'informatique et les nouvelles technologies, je suis également un grand amateur de cuisine.',
-        link: 'https://www.epita.fr',
-        linkText: 'AI',
-    },
+  {
+    id: 'website',
+    image: new URL('../assets/images/epita.png', import.meta.url).href,
+    title: 'EPITA',
+    description:
+      'Actuellement étudiant en informatique à EPITA, je me spécialise en Intelligence Artificielle et Machine Learning.',
+    link: 'https://www.epita.fr',
+    linkText: 'EPITA'
+  },
+  {
+    id: 'ai',
+    image: new URL('../assets/images/simon_gray.png', import.meta.url).href,
+    title: 'Intelligence Artificielle',
+    description:
+      "Passionné par l'informatique et les nouvelles technologies, je suis également un grand amateur de cuisine.",
+    link: 'https://www.epita.fr',
+    linkText: 'AI'
+  }
 ]);
+
+function handleClick(id) {
+    console.log('clicked on', id); 
+}
 </script>
 
 <template>
-    <div class="work">
-        <h2 class="work-title">Mes travaux</h2>
-        <div class="work-cards">
-            <SmallCard
-                v-for="project in projects"
-                :key="project.id"
-                :image="project.image"
-                :title="project.title"
-                :description="project.description"
-                :link="project.link"
-                :linkText="project.linkText"
-            />
-        </div>
+  <div class="work">
+    <div class="work-cards">
+      <SmallCard
+        v-for="project in projects"
+        @cardClicked="handleClick"
+        :key="project.id"
+        :title="project.title"
+        :description="project.description"
+        :image="project.image"
+        :link="project.link"
+        :linkText="project.linkText"
+        :id="project.id"
+      />
     </div>
+  </div>
 </template>
+
+<style>
+@import '../assets/views/work.css';
+</style>
